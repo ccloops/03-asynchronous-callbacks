61d08c6de58fcf5f86a542748ff7b8379896e04a
Catherine Looper <loopercat@gmail.com>
2017-11-29 14:49:27 -0800

    Added tests that pass for accessing an array of .txt information through an array of filepaths

--- lab-catherine/__test__/reader.test.js
+++ lab-catherine/__test__/reader.test.js
@@ -2,20 +2,19 @@
 
 const textReader = require('../lib/reader');
 
+const paths = [`${__dirname}/../assets/cat.txt`, `${__dirname}/../assets/elephant.txt`,`${__dirname}/../assets/seal.txt`];
+
 describe('reader.test.js', () => {
-  test('return the cat.js file if there are no errors',
-    (done) => {
-      textReader.readText((error, data) => {
-        expect(error).toBeNull();
-        expect(data).toEqual('Annoy owner until he gives you food say meow repeatedly until belly rubs, feels good vomit food and eat it again yet lay on arms while you\'re using the keyboard purr yet i could pee on this if i had the energy. Howl on top of tall thing. ');
-        done();
+
+  describe('testing to see if file text and file paths are displaying correctly', () => {
+
+    test('return the file text if there are no errors',
+      (done) => {
+        textReader.readText(paths, (error, data) => {
+          expect(error).toBeNull();
+          expect(data).toEqual([`Cats say meow!`, `Elephants are the largest land animals in the world.`, `Seals spend much of their life in water, but they mate, give birth to babies and take care of them on the shore.`]);
+          done();
+        });
       });
-    });
+  });
 });
-
-// Reader Module Tests
-
-// Use TDD describe and test methods to define descriptive tests
-// Each test callback should aim to test a small well defined feature of a function
-// Write tests to ensure the reader function rejects errors with invalid file paths
-// Write tests to ensure the reader function correctly resolves mapped string data for an array of file paths
--- lab-catherine/lib/reader.js
+++ lab-catherine/lib/reader.js
@@ -3,20 +3,20 @@
 const textReader = module.exports = {};
 const fs = require('fs');
 
-textReader.readText = (callback) => {
-  let catFilePath = `${__dirname}/../assets/cat.txt`;
-  fs.readFile(catFilePath, (error, data) => {
-    if(error)
-      callback(error);
-    callback(null, data.toString());
+const fileTextArray = [];
+
+textReader.readText = (paths, callback) => {
+  fs.readFile(paths[0], (error, data) => {
+    if(error) callback(error);
+    fileTextArray.push(data.toString());
+    fs.readFile(paths[1], (error, data) => {
+      if(error) callback(error);
+      fileTextArray.push(data.toString());
+      fs.readFile(paths[2], (error, data) => {
+        if(error) callback(error);
+        fileTextArray.push(data.toString());
+        callback(null, fileTextArray);
+      });
+    });
   });
 };
-
-
-// Reader Module
-
-// In the lib/ directory create a reader.js module that exports a single function. The reader module should take an array of three file paths and resolve a mapped array of strings loaded from each file using an error-first callback. The string data should be in the same order as the file path data (mapped). If an error occurs it should immediately reject the error using the callback and stop execution.
-
-// The reader module should have the function signature (paths, callback) => undefined
-// On a failure the reader module should invoke the callback with an error callback(error)
-// On success the reader module should invoke the callback with null as the first parameter, and the result as the second parameter callback(null, result)
