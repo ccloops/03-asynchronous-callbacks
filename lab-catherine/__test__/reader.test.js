106916a4fca7007c93049790b0a0788dabd2a2c5
Catherine Looper <loopercat@gmail.com>
2017-11-29 13:47:17 -0800

    Tested cat.txt file to see that code is functioning properly

--- lab-catherine/__test__/reader.test.js
+++ lab-catherine/__test__/reader.test.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const textReader = require('../lib/reader');
+
+describe('reader.test.js', () => {
+  test('return the cat.js file if there are no errors',
+    (done) => {
+      textReader.readText((error, data) => {
+        expect(error).toBeNull();
+        expect(data).toEqual('Annoy owner until he gives you food say meow repeatedly until belly rubs, feels good vomit food and eat it again yet lay on arms while you\'re using the keyboard purr yet i could pee on this if i had the energy. Howl on top of tall thing. ');
+        done();
+      });
+    });
+});
+
+// Reader Module Tests
+
+// Use TDD describe and test methods to define descriptive tests
+// Each test callback should aim to test a small well defined feature of a function
+// Write tests to ensure the reader function rejects errors with invalid file paths
+// Write tests to ensure the reader function correctly resolves mapped string data for an array of file paths
--- lab-catherine/lib/reader.js
+++ lab-catherine/lib/reader.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const textReader = module.exports = {};
+const fs = require('fs');
+
+textReader.readText = (callback) => {
+  let catFilePath = `${__dirname}/../assets/cat.txt`;
+  fs.readFile(catFilePath, (error, data) => {
+    if(error)
+      callback(error);
+    callback(null, data.toString());
+  });
+};
+
+
+// Reader Module
+
+// In the lib/ directory create a reader.js module that exports a single function. The reader module should take an array of three file paths and resolve a mapped array of strings loaded from each file using an error-first callback. The string data should be in the same order as the file path data (mapped). If an error occurs it should immediately reject the error using the callback and stop execution.
+
+// The reader module should have the function signature (paths, callback) => undefined
+// On a failure the reader module should invoke the callback with an error callback(error)
+// On success the reader module should invoke the callback with null as the first parameter, and the result as the second parameter callback(null, result)
